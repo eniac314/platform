52a55a7f7384080d7c6d2ae76380937d5a1f82be
fayong <eniac314@gmail.com>
2018-01-22 12:11:33 +0100

    json api

--- lib/platform/accounts/player.ex
+++ lib/platform/accounts/player.ex
@@ -1,10 +1,14 @@
 defmodule Platform.Accounts.Player do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Platform.Products.Gameplay
+  alias Platform.Products.Game
   alias Platform.Accounts.Player
 
 
   schema "players" do
+    many_to_many :games, Game, join_through: Gameplay
+
     field :display_name, :string
     field :password, :string, virtual: true
     field :password_digest, :string
@@ -13,6 +17,8 @@ defmodule Platform.Accounts.Player do
 
     timestamps()
   end
+  
+  
 
   @doc false
   def changeset(%Player{} = player, attrs) do
@@ -20,5 +26,32 @@ defmodule Platform.Accounts.Player do
     |> cast(attrs, [:display_name, :password, :username, :score])
     |> validate_required([:username])
     |> unique_constraint(:username)
+    |> validate_length(:username, min: 2, max: 100)
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_digest()
   end
+
+  @doc false
+  def registration_changeset(%Player{} = player, attrs) do
+    player 
+    |> cast(attrs, [:password, :username])
+    |> validate_required([:password, :username])
+    |> unique_constraint(:username)
+    |> validate_length(:username, min: 2, max: 100)
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_digest()
+  end
+
+
+  defp put_pass_digest(changeset) do
+    case changeset do 
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_digest, Comeonin.Bcrypt.hashpwsalt(pass))
+
+      _ -> changeset
+    end
+  end
+
 end
+
+
